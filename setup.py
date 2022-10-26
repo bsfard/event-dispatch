7ef21377cf812426ae567553a5f0bcb2c642dadb
bsfard <38926677+bsfard@users.noreply.github.com>
2022-10-26 10:13:39 -0700

    Setting up project with setup.py and top-level package in preparation for pip install.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eventdispatch',
+    version='0.0.1',
+    author='Ben Sfard',
+    author_email='bsfard@gmail.com',
+    url='https://github.com/bsfard/event-dispatch',
+    packages=find_packages(),
+)

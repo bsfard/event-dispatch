51b320541554ec526acebd7cb20ac5dc06392b4d
bsfard <38926677+bsfard@users.noreply.github.com>
2022-12-09 10:04:42 -0800

    Adding EventDispatchEvent to top level package for convenience.

--- eventdispatch/__init__.py
+++ eventdispatch/__init__.py
@@ -1,17 +1,15 @@
+from .core import Data as Data
+from .core import Event as Event
+from .core import EventDispatch as EventDispatch
+from .core import EventDispatchEvent as EventDispatchEvent
+from .core import InvalidDataError as InvalidDataError
+from .core import InvalidEventError as InvalidEventError
+from .core import MissingKeyError as MissingKeyError
 from .core import NamespacedEnum as NamespacedEnum
+from .core import NotifiableError as NotifiableError
 from .core import post_event as post_event
 from .core import register_for_events as register_for_events
 from .core import unregister_from_events as unregister_from_events
-
-from .core import NotifiableError as NotifiableError
-from .core import InvalidDataError as InvalidDataError
-from .core import MissingKeyError as MissingKeyError
-from .core import InvalidEventError as InvalidEventError
-
-from .core import EventDispatch as EventDispatch
-from .core import Event as Event
-from .core import Data as Data
-
+from .properties import ImmutablePropertyModificationError as ImmutablePropertyModificationError
 from .properties import Properties
 from .properties import PropertyNotSetError as PropertyNotSetError
-from .properties import ImmutablePropertyModificationError as ImmutablePropertyModificationError

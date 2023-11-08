9cbe264fb8a3957f5d71ab2d6736e721dd48eede
bsfard <38926677+bsfard@users.noreply.github.com>
2023-11-08 00:05:29 -0800

    Updated demo workers with event mapping example. Also created Worker base class for common worker functionality.  Improved demo logging.

--- demo/run_workers.py
+++ demo/run_workers.py
@@ -1,15 +1,28 @@
 import logging
 import time
 
-from demo.workers import Worker1, Worker2, WorkerEvent
+from demo.workers import Worker1, WorkerEvent, Worker2, Worker3
 from eventdispatch import post_event
 
-logging.basicConfig(level=logging.INFO)
+LOG_LEVEL = logging.INFO
+
+
+def set_up_logger():
+    formatter = logging.Formatter('[%(asctime)s - %(levelname)s - %(name)s] %(message)s')
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logging.getLogger().addHandler(ch)
+    logging.getLogger().setLevel(LOG_LEVEL)
+
+
+set_up_logger()
 
 Worker1()
 Worker2()
+Worker3()
 
 # Generate initial event to kick things off.
 post_event(WorkerEvent.APP_STARTED)
 
+# Give demo workers time to finish.
 time.sleep(6)
--- demo/workers.py
+++ demo/workers.py
@@ -1,12 +1,11 @@
+import json
 import logging
 import time
-from typing import Any
 
-from eventdispatch import Event, register_for_events, unregister_from_events, post_event, NamespacedEnum
+from eventdispatch import Event, register_for_events, unregister_from_events, post_event, NamespacedEnum, Properties, \
+    map_events
 
 STEP_SIM_WORK_SEC = 1
-
-logging.basicConfig(level=logging.INFO)
 
 
 class WorkerEvent(NamespacedEnum):
@@ -15,23 +14,55 @@ class WorkerEvent(NamespacedEnum):
     STEP2_COMPLETED = 'step2_completed'
     STEP3_COMPLETED = 'step3_completed'
     STEP4_COMPLETED = 'step4_completed'
+    ALL_STEPS_COMPLETED = 'all_steps_completed'
 
     def get_namespace(self) -> str:
         return 'worker'
 
 
-class Worker1:
-    desired_events = [
+class Worker:
+    def __init__(self, events_to_watch: [WorkerEvent]):
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+        self.events_to_watch = events_to_watch
+        self.is_pretty_print = Properties().has('CLIENT_LOGGING_PRETTY_PRINT') and \
+                               Properties().get('CLIENT_LOGGING_PRETTY_PRINT')
+
+        register_for_events(self.on_event, self.events_to_watch)
+
+    def on_event(self, event: Event):
+        self.log_event(event)
+
+    @staticmethod
+    def wait(amount: float):
+        time.sleep(amount)
+
+    def log_task(self, task_name: str):
+        self.logger.info(f'Doing: {task_name}\n')
+
+    def log_event(self, event: Event):
+        if self.is_pretty_print:
+            payload = json.dumps(event.dict, indent=2)
+        else:
+            payload = event.dict
+        self.logger.info(f"Got event '{event.name}'\n{payload}\n")
+
+    def unregister(self):
+        unregister_from_events(self.on_event, self.events_to_watch)
+
+
+class Worker1(Worker):
+    events_to_watch = [
         WorkerEvent.APP_STARTED,
         WorkerEvent.STEP2_COMPLETED,
         WorkerEvent.STEP3_COMPLETED,
     ]
 
     def __init__(self):
-        register_for_events(self.on_event, self.desired_events)
+        super().__init__(self.events_to_watch)
 
     def on_event(self, event: Event):
-        log_event(self, event)
+        super().on_event(event)
 
         # Map events that occurred to action to perform.
         if event.name == WorkerEvent.APP_STARTED.namespaced_value:
@@ -42,31 +73,31 @@ class Worker1:
 
         elif event.name == WorkerEvent.STEP3_COMPLETED.namespaced_value:
             # Done (cleanup).
-            unregister_from_events(self.on_event, self.desired_events)
+            self.unregister()
 
     def do_step1(self):
-        log_task(self, 'step 1')
-        wait(STEP_SIM_WORK_SEC)
-        post_event(WorkerEvent.STEP1_COMPLETED)
+        self.log_task('step 1')
+        self.wait(STEP_SIM_WORK_SEC)
+        post_event(WorkerEvent.STEP1_COMPLETED, {'id': 10})
 
     def do_step3(self):
-        log_task(self, 'step 3')
-        wait(STEP_SIM_WORK_SEC)
+        self.log_task('step 3')
+        self.wait(STEP_SIM_WORK_SEC)
         post_event(WorkerEvent.STEP3_COMPLETED)
 
 
-class Worker2:
-    desired_events = [
+class Worker2(Worker):
+    events_to_watch = [
         WorkerEvent.STEP1_COMPLETED,
         WorkerEvent.STEP3_COMPLETED,
         WorkerEvent.STEP4_COMPLETED,
     ]
 
     def __init__(self):
-        register_for_events(self.on_event, self.desired_events)
+        super().__init__(self.events_to_watch)
 
     def on_event(self, event: Event):
-        log_event(self, event)
+        super().on_event(event)
 
         # Map events that occurred to action to perform.
         if event.name == WorkerEvent.STEP1_COMPLETED.namespaced_value:
@@ -77,30 +108,40 @@ class Worker2:
 
         elif event.name == WorkerEvent.STEP4_COMPLETED.namespaced_value:
             # Done (cleanup).
-            unregister_from_events(self.on_event, self.desired_events)
+            self.unregister()
 
     def do_step2(self):
-        log_task(self, 'step 2')
-        wait(STEP_SIM_WORK_SEC)
+        self.log_task('step 2')
+        self.wait(STEP_SIM_WORK_SEC)
         post_event(WorkerEvent.STEP2_COMPLETED)
 
     def do_step4(self):
-        log_task(self, 'step 4')
-        wait(STEP_SIM_WORK_SEC)
-        post_event(WorkerEvent.STEP4_COMPLETED)
+        self.log_task('step 4')
+        self.wait(STEP_SIM_WORK_SEC)
+        post_event(WorkerEvent.STEP4_COMPLETED, {'age': 25})
 
 
-def wait(amount: float):
-    time.sleep(amount)
+class Worker3(Worker):
+    events_to_watch = [
+        WorkerEvent.ALL_STEPS_COMPLETED
+    ]
 
+    def __init__(self):
+        super().__init__(self.events_to_watch)
 
-def log_task(for_class: Any, task_name: str):
-    get_logger(for_class).info(f' Doing: {task_name}\n')
+        map_events(
+            events_to_map=[
+                Event(WorkerEvent.STEP1_COMPLETED, {'id': 10}),
+                Event(WorkerEvent.STEP2_COMPLETED),
+                Event(WorkerEvent.STEP3_COMPLETED),
+                Event(WorkerEvent.STEP4_COMPLETED, {'age': 25})
+            ],
+            event_to_post=Event(WorkerEvent.ALL_STEPS_COMPLETED, {'message': 'hello'})
+        )
 
+    def on_event(self, event: Event):
+        super().on_event(event)
 
-def log_event(for_class: Any, event: Event):
-    get_logger(for_class).info(f" Got event '{event.name}'\n{event.dict}\n")
-
-
-def get_logger(cls: Any):
-    return logging.getLogger(cls.__class__.__name__)
+        if event.name == WorkerEvent.ALL_STEPS_COMPLETED.namespaced_value:
+            # Done (cleanup).
+            self.unregister()
